e5f6729e988dfd17e4aa1961f195150dd0508468
Junyan Feng <jf4044@nyu.edu>
2022-10-07 01:01:57 +0800

    nextjs initialization

---
+++ nextjs/pages/_app.js
@@ -0,0 +1,6 @@
+//import global css here
+import '../styles/global.css'
+
+export default function App({ Component, pageProps }) {
+    return <Component {...pageProps} />;
+  }
---
+++ nextjs/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p style={{textAlign:'center'}}>The Community For Horse Racing Lovers</p>
+      </section>
+    </Layout>
+  );
+}
