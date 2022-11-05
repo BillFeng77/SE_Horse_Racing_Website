5a1e06ae2412bf665b85d8a48ac3e7ddfbf4a1f5
Junyan Feng <jf4044@nyu.edu>
2022-11-05 18:46:20 +0800

    add backend

---
+++ backend/server.py
@@ -0,0 +1,54 @@
+import json
+from flask import Flask, request, render_template
+from flask_cors import cross_origin, CORS
+from flask_pymongo import PyMongo
+from flask.json import jsonify
+
+
+app = Flask(__name__)
+CORS(app)
+app.config['MONGO_URI'] = "mongodb+srv://Pat:13008391115Sjc@cluster0.oamulll.mongodb.net/Horse_information_dataset_v1"
+mongo=PyMongo(app)
+
+@app.route('/')
+def index():
+    return "<h1 > Home page </h1>"
+
+#send data to frontend by "serversideprops"
+@app.route('/user')
+def user():
+    return json.dumps({'name': 'alice',
+                       'email': 'alice@outlook.com'})
+
+#send data to frontend by axios
+@app.route('/news')
+def news():
+    return json.dumps({'title': 'derby',
+                       'content': 'it wins'})
+
+#get data from frontend
+@app.route('/api/query',methods = ['POST'])
+#@cross_origin(origins='http://localhost:3000')
+def get_query_from_react():
+    data = request.form
+    newdata=json.dumps(data)
+    print(newdata)
+    return newdata
+
+@app.route('/name', methods=['GET', 'POST'])
+def get_dates():
+    sd = request.form.get('first')
+    print(sd)
+    return
+
+@app.route('/mongouser')
+def get_data_from_mongodb():
+    horse_info_db=mongo.db
+    #insert=horse_info_db["UserList"].insert_one({"email":"1111","password":"2222","userName":"2233"})
+
+    #find
+    #find=horse_info_db["UserList"].find()
+    return
+
+if __name__=='__main__':
+    app.run(debug=True)
