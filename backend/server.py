a5dbcfeef7c071dfe27a356b8067416cb479ad30
Jennifer-Yu1206 <jny223@nyu.edu>
2022-11-13 03:22:02 +0800

    fixed typeerror, api 400 & 500 error

--- backend/server.py
+++ backend/server.py
@@ -82,16 +82,23 @@ def get_messages_from_mongodb():
 @app.route('/messages', methods=['POST'])
 def insert_message_to_mongodb():
     db = mongo.db
-    data = request.get_json()
-    id = db["Comments"].insert_one(data).inserted_id
-    # get和使用id时 出现typeError  不影响使用
-    # TypeError: Object of type ObjectId is not JSON serializable     UNRESOLVED ERROR HERE!!!!
+    data = request.form
+    newdata = json.dumps(data)  # type: str
+    doc = json.loads(newdata)  # str to json
+    id = db["Comments"].insert_one(doc).inserted_id
     counter = update_counter_forum()
     db["Comments"].update_one(
-        {'_id': ObjectId(id)}, {"$set": {'count': counter}})
-    return data
+        {'_id': ObjectId(id)}, {"$set": {'count': counter}})  # assign new id based on 评论的先后顺序
+    db["Comments"].update_one(
+        {'_id': ObjectId(id)}, {"$set": {'likes': 0}})
+    db["Comments"].update_one(
+        {'_id': ObjectId(id)}, {"$set": {'dislikes': 0}})
+
+    # print('request.form:', request.form)    all data in string type
+    return newdata
 
 
+# generate a increasing number as the new id of messages in forum, according to 评论的先后顺序
 def update_counter_forum():
     db = mongo.db
     db["Counters_Messages_Forum"].find_one_and_update(
