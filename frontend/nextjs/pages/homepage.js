869868a3dd552a40992deac5ab3307e6dda9e09e
Patrickhshs <js12556@nyu.edu>
2022-11-14 09:55:27 +0800

    Add carousel

--- frontend/nextjs/pages/homepage.js
+++ frontend/nextjs/pages/homepage.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Menu from '../components/menu';
 import calender from '../components/calendar';
 import {Layout} from 'antd';
+import carousel from '../components/Carousel';
 const {Header, Footer, Sider, Content}=Layout;
 const homePage=()=>{
     return (
         <Layout>
             <Header>
                 <Menu></Menu>
+                <carousel></carousel>
             </Header>
             <Content>
 
