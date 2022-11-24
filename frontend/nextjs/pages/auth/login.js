209b48362bbdcb60be2b4555c2e579f3ee09297b
Junyan Feng <jf4044@nyu.edu>
2022-11-24 16:36:20 +0800

    login page ui

--- frontend/nextjs/pages/auth/login.js
+++ frontend/nextjs/pages/auth/login.js
@@ -1,17 +1,32 @@
 
-import Menu from '../../components/menu';
+import Menu from '../../components/menu'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
 
 function loginpage(){
     return (
         <>
         <Menu />
-        <form action="/api/login" method="post">
-            <label for="first">First name:</label>
-            <input type="text" id="first" name="first" />
-            <label for="last">Last name:</label>
-            <input type="text" id="last" name="last" />
-            <button type="submit">Submit</button>
-        </form>
+        <Box
+            //sx={{width:200}}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            component="form"
+            justify="center"
+            noValidate
+            autoComplete="off"
+            >
+            <h1 style={{color:'darkred'}}>Login For More Fun!</h1>
+            <TextField id="standard-basic" label="Email" variant="standard" />
+            <br/>
+            <TextField id="standard-basic" label="Password" variant="standard" />
+            <Button color='primary' variant='contained' sx={{marginTop:4}}>Login</Button>
+            <Link sx={{marginTop:4}} href="#">No account? Please register here</Link>
+        </Box>
         </>
     )
 }
