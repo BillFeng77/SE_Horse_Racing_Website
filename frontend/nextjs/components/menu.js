d032a0871587dcbb4315c776649df816884e6226
Patrickhshs <js12556@nyu.edu>
2022-11-29 20:03:07 +0800

    WIP: Menu dropdown

--- frontend/nextjs/components/menu.js
+++ frontend/nextjs/components/menu.js
@@ -1,11 +1,60 @@
 import Link from 'next/link';
-
+import React from 'react';
+import {Dropdown,Space} from 'antd';
 import styles from '../styles/menu.module.css';
+import {Menu} from'antd';
 
 
-export default function Menu() {
+export default function Definemenu() {
+    const userItems=[{key:'1',label:(<a target="_blank" rel="noopener noreferrer" href='/auth/logout'>Logout</a>)},
+    {key:"2",label:(<a target="_blank" rel="noopener noreferrer" href="admin-manage-accounts">Admin Login</a>)}]
     return (
-        <div className={styles.header}>
+        <Menu mode="horizontal" className={styles.header}title="Horse Racing">
+            <li>
+                <a>Horse racing</a>
+            </li>
+            <Menu.Item className={styles.item}>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item className={styles.item}>
+                <Link href="/horses">
+                    <a>Horse</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item className={styles.item}>
+                <Link href="/forum">
+                    <a>Forum</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item className={styles.item}>
+                <Link href="/forum">
+                    <a>Forum</a>
+                </Link>
+            </Menu.Item>
+            <Menu.Item className={styles.item}>
+                <Link href="/auth/login">
+                    <a>Login</a>
+                </Link>
+            </Menu.Item>
+            <Dropdown>
+                
+            </Dropdown>
+            <Menu.SubMenu title="Hi user" className={styles.item}>
+                <Menu.Item>
+                    <Link href="/auth/logout">
+                        <a>Logout</a>
+                    </Link>
+                </Menu.Item>
+                <Menu>
+                    <Link href="admin-manage-accounts">
+                        <a>Admin management</a>
+                    </Link>
+                </Menu>
+            </Menu.SubMenu>
+        </Menu>
+        /*<div className={styles.header}>
             <b className={styles.title}>Horseracing</b>
         <div className={styles.nav}>
             <ul className={styles.list}>
@@ -32,9 +81,6 @@ export default function Menu() {
             </ul>
         </div>
         <div className={styles.userName}>
-            {/* TODO: drop down 组件, 包括logout，administrator link*/}
-            <p>Hi, user!</p>
-        </div>
-        </div>
+            {/* TODO: Login 完善后实时显示用户名字 */
     );
 }
