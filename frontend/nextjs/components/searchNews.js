11dd64a9e97dd618ee6fa2b4f961f51b5a3437a0
Patrickhshs <js12556@nyu.edu>
2022-12-09 23:29:19 +0800

    Fix search

--- frontend/nextjs/components/searchNews.js
+++ frontend/nextjs/components/searchNews.js
@@ -10,7 +10,7 @@ export default function Search() {
     }
   const onSubmit = () => {
     // make a post request to the Flask server with the search query
-    axios.get('http://127.0.0.1:5000/api/search?q=${query}')
+    axios.get(`http://127.0.0.1:5000/api/search?q=${query}`)
       .then(response =>{ 
         // update the state with the search results
         setResults(response.data);
@@ -27,7 +27,7 @@ export default function Search() {
     <button onClick={onSubmit}>Search</button>
     <ul>
       {results.map(result => 
-        <li key={result._id}><Link href={`/news/${news.title}`}>{result.title}</Link></li>
+        <li key={result._id}><Link href={`/news/${result.title}`}>{result.title}</Link></li>
 )}
     </ul>
   </div>
