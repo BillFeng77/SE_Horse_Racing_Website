9ab3124593f85900ec7297b64414466505f31cfc
Jennifer-Yu1206 <jny223@nyu.edu>
2022-12-10 22:50:17 +0800

    search api refine

--- backend/search.py
+++ backend/search.py
@@ -1,13 +1,16 @@
-from flask import Flask,jsonify
+from flask import Flask, jsonify
 from flask import request
 from config import app, mongo
 from bson.json_util import dumps
 db = mongo.db
+
+
 @app.route('/api/search')
 def search():
-    query=request.args.get("q")
-    results= db["News"].find(
-      { 'title': { '$regex': query } }
-     )
-    data=dumps(list(results))
+    query = request.args.get("q")
+    query = query.title()
+    results = db["News"].find(
+        {'title': {'$regex': query}}
+    )
+    data = dumps(list(results))
     return data
